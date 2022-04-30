7f590c75070239cc646100fa7bd9c5a46f324df8
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-04-30 22:13:37 +0900

    add init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>{{post}}</div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'AboutView',
+  data(){
+    return{
+      post:{}
+    }
+  },
+  created() {
+    axios.get('http://localhost:30000/api/board')
+        .then(async res=>{
+          const resData = await res.data
+          console.log(resData)
+          this.post = resData;
+        })
+        .catch()
+  }
+}
+</script>
