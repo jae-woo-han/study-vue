8d601058d42136241cbe6d68ce75014923dc6398
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-05-02 22:40:42 +0900

    add PostView, Components

---
+++ src/views/PostView.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+import Post from "@/components/Post";
+import FileList from "@/components/FileList";
+import CommentList from "@/components/CommentList";
+
+export default {
+  name: "PostView",
+  components:{
+    Post,
+    FileList,
+    CommentList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
