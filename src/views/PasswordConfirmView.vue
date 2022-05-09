a1b5674460af60240e488ddb4e321ee1559ac0cc
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-05-09 21:09:45 +0900

    add view components

---
+++ src/views/PasswordConfirmView.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "PasswordConfireView"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/PostUpdateView.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "PostUpdateView"
+}
+</script>
+
+<style scoped>
+
+</style>
