caa1667a2a4f8527587ad220b6cb23a371ab80e7
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-06-08 22:58:24 +0900

    add new FileForm

---
+++ src/components/CreateFileBlob.vue
@@ -0,0 +1,17 @@
+<template>
+    <input type="file" v-on:change="appendFile">
+</template>
+
+<script>
+//파일 추가 컴포넌트
+//부모 컴포넌트의 appendFile 함수로 파일 객체를 보냄
+export default{
+    name:"CreateFileBlob",
+    methods:{
+      appendFile: function (event) {
+        let fileList = event.target.files;
+        this.$emit("appendFile",fileList.item(0));
+      }
+    }
+}
+</script>
