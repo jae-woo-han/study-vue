3f3e12b91791bb31539c6b2373522901fd35c447
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-06-09 23:13:50 +0900

    이름 변경

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -6,7 +6,7 @@
 //파일 추가 컴포넌트
 //부모 컴포넌트의 appendFile 함수로 파일 객체를 보냄
 export default{
-    name:"CreateFileBlob",
+    name:"FileInput",
     methods:{
       appendFile: function (event) {
         let fileList = event.target.files;
