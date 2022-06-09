10afd5f4b739bbd5ab5bc82c99c7c72e76c5ade0
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-06-09 23:14:04 +0900

    fix v-on shorthand

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -1,7 +1,8 @@
 <template>
   <ul>
-    <li v-for="file in fileList" v-on:click="downloadFile(file)" v-bind:key="file.fileSeq">
+    <li v-for="file in fileList" @click="downloadFile(file)" v-bind:key="file.fileSeq">
       {{ file.fileName }}.{{ file.fileType }}
+      <font-awesome-icon icon="fa-solid fa-xmark" @click="" v-if="isUpdate"/>
     </li>
   </ul>
 </template>
@@ -12,6 +13,7 @@ import {getFileList} from "@/service/api/FileService";
 
 export default {
   name: "FileList",
+  props:["isDownload","isUpdate"],
   data() {
     return {
       fileList: []
